ca6e800ef514a37e939a3d0e0ba5ddbb910d2ffa
pavankumar potta <pavankumar.potta1998@gmail.com>
2023-03-25 13:34:02 -0400

    implemented PIMPL in code

--- PIMPL_DLL/StableClass.cpp
+++ PIMPL_DLL/StableClass.cpp
@@ -1,34 +1,32 @@
-// PIMPL_DLL.cpp : Defines the exported functions for the DLL.
-//
-
 #include "framework.h"
 #include "StableClass.h"
 #include "SomeContainedClass1.h"
 #include "SomeContainedClass2.h"
 
+// Private implementation class definition
+class StableClassImpl {
+public:
+    SomeContainedClass1* m_SomeContainedClass1;
+    SomeContainedClass2* m_SomeContainedClass2;
+};
 
-
-StableClass::StableClass(void) : m_SomeContainedClass1(nullptr), m_SomeContainedClass2(nullptr)
+StableClass::StableClass(void) : m_Impl(new StableClassImpl())
 {
+    m_Impl->m_SomeContainedClass1 = nullptr;
+    m_Impl->m_SomeContainedClass2 = nullptr;
+}
 
+StableClass::~StableClass(void)
+{
+    delete m_Impl;
 }
 
 void StableClass::Method1(int i)
 {
-
+    // Method1 implementation
 }
 
 void StableClass::Method2(int i)
 {
-
-}
-
-SomeContainedClass1* StableClass::GetSomeContainedClass1()
-{
-    return m_SomeContainedClass1;
-}
-
-SomeContainedClass2* StableClass::GetSomeContainedClass2()
-{
-    return m_SomeContainedClass2;
+    // Method2 implementation
 }
--- PIMPL_DLL/StableClass.h
+++ PIMPL_DLL/StableClass.h
@@ -1,26 +1,17 @@
-
 #include "PIMPLDLL_EXPORTS.h"
 #include "BaseStableClass.h"
 
-
-class SomeContainedClass1;
-class SomeContainedClass2;
+class StableClassImpl; // Forward declaration of the private implementation class
 
 // This class is exported from the dll
 class PIMPLDLL_API StableClass : BaseStableClass {
 public:
 	StableClass(void);
+	~StableClass(void);
 
-	void Method1(int i) override; 
-	void Method2(int i) override; // Newly Added
-
-	SomeContainedClass1 * GetSomeContainedClass1();
-	SomeContainedClass2 * GetSomeContainedClass2();
+	void Method1(int i);
+	void Method2(int i);
 
 private:
-	SomeContainedClass1* m_SomeContainedClass1;
-	SomeContainedClass2* m_SomeContainedClass2; // Newly Added
-
+	StableClassImpl* m_Impl;
 };
-
-
